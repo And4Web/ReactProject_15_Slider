bdfaad2c27403be1af03f6e7185c655800ebb8eb
And4Web <anands2807@gmail.com>
2022-05-30 20:16:57 +0530

    project finished. implemented all the functionality, copied the css file from github.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,70 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import data from "./data";
-
 function App() {
-  const [user, setUser] = useState(data);
-  const [index, setIndex] = useState(0);
-  console.log(user);
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
 
   return (
-    <div className="App">
-      <h1>Slider app</h1>
-      <section style={{ position: "relative" }}>
-        {user.map((person) => {
-          const { id, name, title, quote, image } = person;
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+
           return (
-            <article
-              key={id}
-              style={{ position: "absolute", top: "10vh", left: "10vw" }}
-            >
-              <img
-                src={image}
-                alt={name}
-                style={{
-                  height: "10rem",
-                  width: "10rem",
-                  borderRadius: "50%",
-                  objectFit: "cover",
-                }}
-              />
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
-              <p>{title}</p>
-              <p>{quote}</p>
-              <FaQuoteRight />
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
             </article>
           );
         })}
-        <button>
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button>
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
 
